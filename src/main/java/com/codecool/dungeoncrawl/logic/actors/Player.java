8da0fefde498e65b667ae588d9fd5efd9b0f82ad
martingrant01 <martin.nagy01@gmail.com>
2021-08-02 19:51:54 +0200

    prep: prepared some methods and fields for inventory

--- src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Player.java
@@ -2,7 +2,13 @@ package com.codecool.dungeoncrawl.logic.actors;
 
 import com.codecool.dungeoncrawl.logic.Cell;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Player extends Actor {
+//    private final HashMap<String, Item> attackItems= new HashMap<>();
+//    private final HashMap<String, Item> defenseItems= new HashMap<>();
+
     public Player(Cell cell) {
         super(cell);
     }
@@ -10,4 +16,25 @@ public class Player extends Actor {
     public String getTileName() {
         return "player";
     }
+
+//    public void addDefensiveItemIntoInventory(Item item) {
+//        defenseItems.put(item);
+//    }
+//
+//    public void addAttackItemIntoInventory(Item item) {
+//        attackItems.put(item);
+//    }
+
+//    public ArrayList<Item> refreshInventory() {
+//        ArrayList<Item> inventory = new ArrayList<Item>();
+//        for (Item attackItem : attackItems) {
+//            inventory.add(attackItem);
+//        }
+//
+//        for (Item defenseItem : defenseItems) {
+//            inventory.add(defenseItem);
+//        }
+//
+//        return inventory;
+//    }
 }
