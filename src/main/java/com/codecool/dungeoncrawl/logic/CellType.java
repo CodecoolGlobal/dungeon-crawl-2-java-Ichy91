17a62fbf990567e6d486ae4c987b3fc241bd957c
saz <szabans@gmail.com>
2021-08-03 14:37:50 +0200

    fix: reduce opened door enums

--- src/main/java/com/codecool/dungeoncrawl/logic/CellType.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/CellType.java
@@ -7,9 +7,7 @@ public enum CellType {
     GREEN_CLOSED_DOOR("green_closed_door"),
     WHITE_CLOSED_DOOR("blue_closed_door"),
     RED_CLOSED_DOOR("red_closed_door"),
-    GREEN_OPENED_DOOR("green_opened_door"),
-    WHITE_OPENED_DOOR("blue_opened_door"),
-    RED_OPENED_DOOR("red_opened_door");
+    OPENED_DOOR("red_opened_door");
 
     private final String tileName;
 
