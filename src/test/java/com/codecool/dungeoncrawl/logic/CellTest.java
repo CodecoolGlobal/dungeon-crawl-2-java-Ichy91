31347e49c37349328c821b88f354144070df984d
Stark Máté <strkmt@gmail.com>
2021-08-17 14:25:02 +0200

    feat: CellTest is complete

--- src/test/java/com/codecool/dungeoncrawl/logic/CellTest.java
+++ src/test/java/com/codecool/dungeoncrawl/logic/CellTest.java
@@ -1,5 +1,7 @@
 package com.codecool.dungeoncrawl.logic;
 
+import com.codecool.dungeoncrawl.logic.actors.Player;
+import com.codecool.dungeoncrawl.logic.items.Armor;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,5 +24,25 @@ class CellTest {
 
         cell = map.getCell(0, 2);
         assertEquals(null, cell.getNeighbor(-1, 1));
+    }
+
+    @Test
+    void cellGettersTest() {
+        Cell cell = new Cell(new GameMap(100,100, CellType.EMPTY), 10, 11, CellType.WALL);
+        assertEquals(CellType.WALL, cell.getType());
+        assertEquals("wall", cell.getTileName());
+        assertEquals(null, cell.getActor());
+        assertEquals(null, cell.getItem());
+    }
+
+    @Test
+    void cellSettersTest() {
+        Cell cell = new Cell(new GameMap(50,50, CellType.FLOOR), 5, 6, CellType.WALL);
+        cell.setType(CellType.EMPTY);
+        cell.setActor(new Player(cell));
+        assertEquals(CellType.EMPTY, cell.getType());
+        assertEquals(cell.getActor().getTileName(), "player");
+        cell.setItem(new Armor(cell));
+        assertEquals(cell.getItem().getTileName(), "armor");
     }
 }
