700379172a2b18ac5d5e0060f0337a957ee28802
Ichy91 <farago.istvan91@gmail.com>
2021-08-17 11:00:21 +0200

    feat: creat inventory table in the database.

--- sample_data/schema_ddl.sql
+++ sample_data/schema_ddl.sql
@@ -10,10 +10,18 @@ DROP TABLE IF EXISTS public.player;
 CREATE TABLE public.player (
     id serial NOT NULL PRIMARY KEY,
     player_name text NOT NULL,
-    hp integer NOT NULL,
-    x integer NOT NULL,
-    y integer NOT NULL
+    hp integer NOT NULL
+);
+
+DROP TABLE IF EXISTS public.inventory;
+CREATE TABLE public.inventory (
+   id serial NOT NULL PRIMARY KEY,
+   player_id integer NOT NULL,
+   type VARCHAR NOT NULL,
+   equipped boolean DEFAULT FALSE
 );
 
 ALTER TABLE ONLY public.game_state
+    ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
+ALTER TABLE ONLY public.inventory
     ADD CONSTRAINT fk_player_id FOREIGN KEY (player_id) REFERENCES public.player(id);
