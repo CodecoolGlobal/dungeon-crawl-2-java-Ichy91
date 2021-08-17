34c68778e273af8e6060d802f8da004416f51b03
saz <szabans@gmail.com>
2021-08-17 16:28:05 +0200

    feat: implement update saved game where name is in db

--- src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
+++ src/main/java/com/codecool/dungeoncrawl/dao/GameStateDaoJdbc.java
@@ -35,6 +35,16 @@ public class GameStateDaoJdbc implements GameStateDao {
 
     @Override
     public void update(GameState state) {
+        try (Connection conn = dataSource.getConnection()){
+            String sql = "UPDATE game_state SET game_state = ?, saved_at = ?  WHERE player_id = ?";
+            PreparedStatement statement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            statement.setString(1, state.getCurrentMap());
+            statement.setDate(2, state.getSavedAt());
+            statement.setInt(3, state.getPlayer().getId());
+            statement.executeUpdate();
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
 
     }
 
