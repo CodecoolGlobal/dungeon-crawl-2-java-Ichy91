a89ba7c45bc0da6eeb2dd5dfee68aface30e8e4c
martingrant01 <martin.nagy01@gmail.com>
2021-08-18 10:39:08 +0200

    feat: pressing buttons on confirmation screen trigger effects

--- src/main/java/com/codecool/dungeoncrawl/Main.java
+++ src/main/java/com/codecool/dungeoncrawl/Main.java
@@ -34,6 +34,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Locale;
+import java.util.Optional;
 
 public class Main extends Application {
     private GameMap map = MapLoader.loadMap("/map2.txt");
@@ -77,17 +78,7 @@ public class Main extends Application {
             exit();
         } else if (saveCombinationMac.match(keyEvent) ||
                 saveCombinationWin.match(keyEvent)) {
-            String nameOfSave = createPopUpWindow();
-            if (nameOfSave != null) {
-                if (dbManager.isNameAlreadyInDB(nameOfSave)) {
-                    createOverwriteWindow();
-                } else {
-                    PlayerModel playerModel = dbManager.savePlayer(player);
-                    dbManager.saveGame(nameOfSave, map.generateFuckingTextFromTheMapState(), playerModel);
-                    dbManager.saveInventory(playerModel, player.getInventory(), player.getEquippedItems());
-                }
-
-            }
+                saveAction();
         }
     }
 
@@ -403,12 +394,31 @@ public class Main extends Application {
         //Creating a dialog
         ButtonType yesButton = new ButtonType("Yes", ButtonBar.ButtonData.OK_DONE);
         ButtonType noButton = new ButtonType("No", ButtonBar.ButtonData.CANCEL_CLOSE);
-        Dialog<String> dialog = new Dialog<>();
+        Dialog<ButtonType> dialog = new Dialog<>();
         dialog.setTitle("Confirmation Tab");
         dialog.setContentText("Would you like to overwrite the already existing state?");
         dialog.getDialogPane().getButtonTypes().add(noButton);
         dialog.getDialogPane().getButtonTypes().add(yesButton);
-        dialog.showAndWait();
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if (result.isPresent() && result.get() == yesButton) {
+            System.out.println("wow");
+        } else if(result.isPresent() && result.get() == noButton) {
+            saveAction();
+        }
+    }
+
+    private void saveAction() {
+        String nameOfSave = createPopUpWindow();
+        if (nameOfSave != null) {
+            if (dbManager.isNameAlreadyInDB(nameOfSave)) {
+                createOverwriteWindow();
+            } else {
+                PlayerModel playerModel = dbManager.savePlayer(player);
+                dbManager.saveGame(nameOfSave, map.generateFuckingTextFromTheMapState(), playerModel);
+                dbManager.saveInventory(playerModel, player.getInventory(), player.getEquippedItems());
+            }
+        }
     }
 
     private void setupDbManager() {
